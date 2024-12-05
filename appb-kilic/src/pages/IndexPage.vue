2f0a989a56b07a55edd01b70e10a22fe082f2717
Noviprofil188 <noviprofil188@gmail.com>
2024-12-05 11:02:56 +0100

    commit

---
+++ appb-kilic/src/pages/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- Logo -->
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+
+    <!-- Prikaz datuma -->
+    <div>
+      <p>Trenutni datum: {{ currentDate }}</p>
+    </div>
+
+    <!-- Prikaz imena i prezimena -->
+    <div>
+      <p>Ime i prezime: {{ fullName }}</p>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+
+// Definiramo podatke za ime, prezime i datum
+const fullName = "Ime Prezime";
+const currentDate = new Date().toLocaleDateString();
+</script>
